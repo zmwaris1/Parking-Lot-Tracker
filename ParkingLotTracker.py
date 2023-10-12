54be658e07b489a81fc2b95e9aa75f23ae3c5579
Zafar Waris <zmwaris1@pop-os.localdomain>
2023-10-12 09:50:50 +0530

    Added Python File

--- ParkingLotTracker.py
+++ ParkingLotTracker.py
@@ -1,63 +1,69 @@
 class ParkingLotTracker:
-
     def __init__(self, MAX_SIZE=40):
-        self.slots = [i for i in range(1, MAX_SIZE+1)]
+        """Constructor method to initialize the parking spots based on MAX_SIZE and a dictionary to add parked vehicles."""
+        self.slots = [i for i in range(1, MAX_SIZE + 1)]
         self.vehicles = {}
 
     def printSlots(self):
+        """Prints the empty slots that could be used for parking."""
         return self.slots
-    
+
     def registerVehicle(self, id):
+        """Registers vehicle id to initiate parking"""
         if len(id) == 0:
-            return 'Enter a valid Vehicle Id'
-        
+            return "Enter a valid Vehicle Id"
+
         if id not in self.vehicles:
             if len(self.slots) > 0:
                 slot = self.slots.pop(0)
                 return self._assignSpot(id, slot)
             else:
-                return 'No parking space'
-        return 'Vehicle already parked.'
-    
+                return "No parking space"
+        return "Vehicle already parked."
+
     def _assignSpot(self, id, spot):
+        """Assigns a parking spot when vehicle is registered."""
         self.vehicles[id] = spot
-        return f'Spot for vehicle {id} is {spot}'
-    
+        return f"Spot for vehicle {id} is {spot}"
+
     def getDetails(self, id):
+        """Extract the details of the vehicle parked in the parking lot."""
         if id in self.vehicles:
             spot = self.vehicles[id]
             if spot <= 20:
-                level = 'A'
+                level = "A"
             else:
-                level = 'B'
-            d = {'level':level, 'spot':spot}
+                level = "B"
+            d = {"level": level, "spot": spot}
             return d
         else:
-            return 'Vehicle not parked here.'
-        
+            return "Vehicle not parked here."
+
     def emptySpot(self, id):
+        """Remove the vehicle from parking lot and display the remaining parking spots."""
         if id in self.vehicles:
             slot = self.vehicles.pop(id)
             self.slots.append(slot)
             self.slots.sort()
             return self.printSlots()
-        return 'Vehicle Id is not present'
-        
-    
+        return "Vehicle Id is not present"
+
+
 obj = ParkingLotTracker()
 
 
 while True:
     user_input = input("Enter Vehicle Id or q to quit: ")
-    if user_input.lower() == 'q':
+    if user_input.lower() == "q":
         break
-    second_input = input("Enter 'R' to register, 'D' to get details of parking, 'E' to empty parking spot: ")
-    if second_input.upper() == 'R':
+    second_input = input(
+        "Enter 'R' to register, 'D' to get details of parking, 'E' to empty parking spot: "
+    )
+    if second_input.upper() == "R":
         print(obj.registerVehicle(user_input))
-    elif second_input.upper() == 'D':
+    elif second_input.upper() == "D":
         print(obj.getDetails(user_input))
-    elif second_input.upper() == 'E':
+    elif second_input.upper() == "E":
         print(obj.emptySpot(user_input))
     else:
-        'Enter Valid Input.'
-
+        "Enter Valid Input."
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Parking-Lot-Tracker
+
+I created this vehicle parking lot project with the OOP concepts using python.
+
+The project consists of the following:
+
+There are 40 spots available in the parking lot, with two levels as 'A' and 'B'.
+
+One class named : ParkingLotTracker.
+
+# Details
+
+Methods used:
+
+1. Constructor method - Initializes the parking spots and a dictionary for already parked vehicle details. Default size is 40.
+
+2. registerVehicle - Vehicle id is given to register the vehicle for parking and a spot is assigned automatically to the vehicle.
+
+3. _assignSpot - assigns parking spot to the vehicle.
+
+4. getDetails - provide details for the parked vehicle i.e the level and the spot of parking. E.g {level: 'A', spot: 19}.
+
+5. emptySpot - empties the already used spot if the vehicle is removed. Add the spot back to the list of parking spots. Also displays the empty spots in the lot.
+
+# How to run
+
+1. Go to the folder where the python file 'ParkingLotTracker.py' is located.
+
+2. Open terminal and enter "python3 ParkingLotTracker.py".
+
+3. Follow the instructions as displayed after execution.
